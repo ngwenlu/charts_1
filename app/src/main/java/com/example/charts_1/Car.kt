c2bd009961d279335d8eba962bbbe4737b0eb528
ngwenlu <97866952+ngwenlu@users.noreply.github.com>
2022-10-19 03:08:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/charts_1/Car.kt
@@ -0,0 +1,13 @@
+package com.example.charts_1
+
+class Car (UID: String? = null,
+           fuelCapacity: Number? = 0,
+           modelName: String? = null,
+           odometer: Double? = null,
+           plateNo: String? = null){
+    var UID = UID
+    var fuelCapacity = fuelCapacity
+    var modelName = modelName
+    var odometer = odometer
+    var plateNo = plateNo
+}
---
+++ app/src/main/java/com/example/charts_1/ChartUtil/BarChartMaker.kt
@@ -0,0 +1,4 @@
+package com.example.charts_1.ChartUtil
+
+class BarChartMaker {
+}
